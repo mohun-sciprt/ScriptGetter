fc9944121207ff0c2650935b5e2b4a52c269968c
mohun-sciprt <biekanwoa@163.com>
2025-02-28 20:20:08 +0800

    Create scripts.lua

---
+++ scripts.lua
@@ -0,0 +1,8 @@
+return{
+    mohun = [[getgenv().mohunkey='MOHUN-Trial'--不要改这个
+getgenv().credits='魔魂脚本作者Discord:yeahhaveagoodtime'--水印
+getgenv().moconfig=true--如果不要自动保存配置就改为false
+pcall(function()loadstring(request{Url='https://raw.githubusercontent.com/mohun-sciprt/MOHUN/refs/heads/main/loader.lua',Method='GET'}.Body)()end)]],
+    baicrack = [[getgenv().crack='魔魂随手破解'
+pcall(function()loadstring(request{Url='https://raw.githubusercontent.com/mohun-sciprt/normal/refs/heads/main/bc.lua',Method='GET'}.Body)()end)]]
+}
